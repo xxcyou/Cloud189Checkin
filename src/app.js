fafef40825d2fde31ab9cf0bf75759debd9bd19c
xxcyou <13250188751@163.com>
2025-02-22 00:59:51 +0800

    添加Ntfy推送

--- src/app.js
+++ src/app.js
@@ -2,24 +2,32 @@ require("dotenv").config();
 const log4js = require("log4js");
 const recording = require("log4js/lib/appenders/recording");
 log4js.configure({
-  appenders: {
-    vcr: {
-      type: "recording",
+    appenders: {
+        vcr: {
+            type: "recording",
+        },
+        out: {
+            type: "console",
+        },
     },
-    out: {
-      type: "console",
+    categories: {
+        default: {
+            appenders: ["vcr", "out"],
+            level: "info"
+        }
     },
-  },
-  categories: { default: { appenders: ["vcr", "out"], level: "info" } },
 });
 
 const logger = log4js.getLogger();
 const superagent = require("superagent");
-const { CloudClient } = require("cloud189-sdk");
+const {
+    CloudClient
+} = require("cloud189-sdk");
 const serverChan = require("./push/serverChan");
 const telegramBot = require("./push/telegramBot");
 const wecomBot = require("./push/wecomBot");
 const wxpush = require("./push/wxPusher");
+const ntfy = require("./push/ntfy");
 const accounts = require("../accounts");
 const families = require("../families");
 const pushPlus = require("./push/pushPlus");
@@ -31,240 +39,281 @@ const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 // 任务 1.签到
 const doUserTask = async (cloudClient) => {
-  const tasks = Array.from({ length: execThreshold }, () =>
-    cloudClient.userSign()
-  );
-  const result = (await Promise.all(tasks)).map(
-    (res) =>
-      `个人任务${res.isSign ? "已经签到过了，" : ""}签到获得${
+    const tasks = Array.from({
+            length: execThreshold
+        }, () =>
+        cloudClient.userSign()
+    );
+    const result = (await Promise.all(tasks)).map(
+        (res) =>
+        `个人任务${res.isSign ? "已经签到过了，" : ""}签到获得${
         res.netdiskBonus
       }M空间`
-  );
-  return result;
+    );
+    return result;
 };
 
 const doFamilyTask = async (cloudClient) => {
-  const { familyInfoResp } = await cloudClient.getFamilyList();
-  if (familyInfoResp) {
-    let familyId = null;
-    //指定家庭签到
-    if (families.length > 0) {
-      const tagetFamily = familyInfoResp.find((familyInfo) =>
-        families.includes(familyInfo.remarkName)
-      );
-      if (tagetFamily) {
-        familyId = tagetFamily.familyId;
-      } else {
-        return [
-          `没有加入到指定家庭分组${families
+    const {
+        familyInfoResp
+    } = await cloudClient.getFamilyList();
+    if (familyInfoResp) {
+        let familyId = null;
+        //指定家庭签到
+        if (families.length > 0) {
+            const tagetFamily = familyInfoResp.find((familyInfo) =>
+                families.includes(familyInfo.remarkName)
+            );
+            if (tagetFamily) {
+                familyId = tagetFamily.familyId;
+            } else {
+                return [
+                    `没有加入到指定家庭分组${families
             .map((family) => mask(family, 3, 7))
             .toString()}`,
-        ];
-      }
-    } else {
-      familyId = familyInfoResp[0].familyId;
-    }
-    logger.info(`执行家庭签到ID:${familyId}`);
-    const tasks = Array.from({ length: execThreshold }, () =>
-      cloudClient.familyUserSign(familyId)
-    );
-    const result = (await Promise.all(tasks)).map(
-      (res) =>
-        `家庭任务${res.signStatus ? "已经签到过了，" : ""}签到获得${
+                ];
+            }
+        } else {
+            familyId = familyInfoResp[0].familyId;
+        }
+        logger.info(`执行家庭签到ID:${familyId}`);
+        const tasks = Array.from({
+                length: execThreshold
+            }, () =>
+            cloudClient.familyUserSign(familyId)
+        );
+        const result = (await Promise.all(tasks)).map(
+            (res) =>
+            `家庭任务${res.signStatus ? "已经签到过了，" : ""}签到获得${
           res.bonusSpace
         }M空间`
-    );
-    return result;
-  }
-  return [];
+        );
+        return result;
+    }
+    return [];
 };
 
 const pushServerChan = (title, desp) => {
-  if (!serverChan.sendKey) {
-    return;
-  }
-  const data = {
-    title,
-    desp,
-  };
-  superagent
-    .post(`https://sctapi.ftqq.com/${serverChan.sendKey}.send`)
-    .type("form")
-    .send(data)
-    .end((err, res) => {
-      if (err) {
-        logger.error(`ServerChan推送失败:${JSON.stringify(err)}`);
+    if (!serverChan.sendKey) {
         return;
-      }
-      const json = JSON.parse(res.text);
-      if (json.code !== 0) {
-        logger.error(`ServerChan推送失败:${JSON.stringify(json)}`);
-      } else {
-        logger.info("ServerChan推送成功");
-      }
-    });
+    }
+    const data = {
+        title,
+        desp,
+    };
+    superagent
+        .post(`https://sctapi.ftqq.com/${serverChan.sendKey}.send`)
+        .type("form")
+        .send(data)
+        .end((err, res) => {
+            if (err) {
+                logger.error(`ServerChan推送失败:${JSON.stringify(err)}`);
+                return;
+            }
+            const json = JSON.parse(res.text);
+            if (json.code !== 0) {
+                logger.error(`ServerChan推送失败:${JSON.stringify(json)}`);
+            } else {
+                logger.info("ServerChan推送成功");
+            }
+        });
 };
 
 const pushTelegramBot = (title, desp) => {
-  if (!(telegramBot.botToken && telegramBot.chatId)) {
-    return;
-  }
-  const data = {
-    chat_id: telegramBot.chatId,
-    text: `${title}\n\n${desp}`,
-  };
-  superagent
-    .post(`https://api.telegram.org/bot${telegramBot.botToken}/sendMessage`)
-    .type("form")
-    .send(data)
-    .end((err, res) => {
-      if (err) {
-        logger.error(`TelegramBot推送失败:${JSON.stringify(err)}`);
+    if (!(telegramBot.botToken && telegramBot.chatId)) {
         return;
-      }
-      const json = JSON.parse(res.text);
-      if (!json.ok) {
-        logger.error(`TelegramBot推送失败:${JSON.stringify(json)}`);
-      } else {
-        logger.info("TelegramBot推送成功");
-      }
-    });
+    }
+    const data = {
+        chat_id: telegramBot.chatId,
+        text: `${title}\n\n${desp}`,
+    };
+    superagent
+        .post(`https://api.telegram.org/bot${telegramBot.botToken}/sendMessage`)
+        .type("form")
+        .send(data)
+        .end((err, res) => {
+            if (err) {
+                logger.error(`TelegramBot推送失败:${JSON.stringify(err)}`);
+                return;
+            }
+            const json = JSON.parse(res.text);
+            if (!json.ok) {
+                logger.error(`TelegramBot推送失败:${JSON.stringify(json)}`);
+            } else {
+                logger.info("TelegramBot推送成功");
+            }
+        });
 };
 
 const pushWecomBot = (title, desp) => {
-  if (!(wecomBot.key && wecomBot.telphone)) {
-    return;
-  }
-  const data = {
-    msgtype: "text",
-    text: {
-      content: `${title}\n\n${desp}`,
-      mentioned_mobile_list: [wecomBot.telphone],
-    },
-  };
-  superagent
-    .post(
-      `https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=${wecomBot.key}`
-    )
-    .send(data)
-    .end((err, res) => {
-      if (err) {
-        logger.error(`wecomBot推送失败:${JSON.stringify(err)}`);
+    if (!(wecomBot.key && wecomBot.telphone)) {
         return;
-      }
-      const json = JSON.parse(res.text);
-      if (json.errcode) {
-        logger.error(`wecomBot推送失败:${JSON.stringify(json)}`);
-      } else {
-        logger.info("wecomBot推送成功");
-      }
-    });
+    }
+    const data = {
+        msgtype: "text",
+        text: {
+            content: `${title}\n\n${desp}`,
+            mentioned_mobile_list: [wecomBot.telphone],
+        },
+    };
+    superagent
+        .post(
+            `https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=${wecomBot.key}`
+        )
+        .send(data)
+        .end((err, res) => {
+            if (err) {
+                logger.error(`wecomBot推送失败:${JSON.stringify(err)}`);
+                return;
+            }
+            const json = JSON.parse(res.text);
+            if (json.errcode) {
+                logger.error(`wecomBot推送失败:${JSON.stringify(json)}`);
+            } else {
+                logger.info("wecomBot推送成功");
+            }
+        });
+};
+
+const puhsNtfy = (title, desp) => {
+    if (!(ntfy.url && ntfy.topic)) {
+        return;
+    }
+    const data = desp;
+    superagent
+        .post(ntfy.url + "/" + ntfy.topic)
+        .set("Title", title)
+        .then((res) => {
+            if (ntfy.url && ntfy.topic) {
+                req.set("Authorization", "Basic " + Buffer.from(ntfy.username + ":" + nfyt.password).toString("base64"));
+            }
+            return res.send(data);
+        })
+        .send(data)
+        .end((err, res) => {
+            if (err) {
+                logger.error(`Ntfy推送失败:${JSON.stringify(err)}`);
+                return;
+            }
+            const json = JSON.parse(res.text);
+            if (!json.id) {
+                logger.error(`Ntfy推送失败:${JSON.stringify(json)}`);
+            } else {
+                logger.info("Ntfy推送成功");
+            }
+        });
 };
 
 const pushWxPusher = (title, desp) => {
-  if (!(wxpush.appToken && wxpush.uid)) {
-    return;
-  }
-  const data = {
-    appToken: wxpush.appToken,
-    contentType: 1,
-    summary: title,
-    content: desp,
-    uids: [wxpush.uid],
-  };
-  superagent
-    .post("https://wxpusher.zjiecode.com/api/send/message")
-    .send(data)
-    .end((err, res) => {
-      if (err) {
-        logger.error(`wxPusher推送失败:${JSON.stringify(err)}`);
+    if (!(wxpush.appToken && wxpush.uid)) {
         return;
-      }
-      const json = JSON.parse(res.text);
-      if (json.data[0].code !== 1000) {
-        logger.error(`wxPusher推送失败:${JSON.stringify(json)}`);
-      } else {
-        logger.info("wxPusher推送成功");
-      }
-    });
+    }
+    const data = {
+        appToken: wxpush.appToken,
+        contentType: 1,
+        summary: title,
+        content: desp,
+        uids: [wxpush.uid],
+    };
+    superagent
+        .post("https://wxpusher.zjiecode.com/api/send/message")
+        .send(data)
+        .end((err, res) => {
+            if (err) {
+                logger.error(`wxPusher推送失败:${JSON.stringify(err)}`);
+                return;
+            }
+            const json = JSON.parse(res.text);
+            if (json.data[0].code !== 1000) {
+                logger.error(`wxPusher推送失败:${JSON.stringify(json)}`);
+            } else {
+                logger.info("wxPusher推送成功");
+            }
+        });
 };
 const pushPlusPusher = (title, desp) => {
-  // 如果没有配置 pushPlus 的 token，则不执行推送
-  if (!(pushPlus.token)) {
-    return;
-  }
-  // 请求体
-  const data = {
-    token: pushPlus.token,
-    title: title,
-    content: desp
-  };
-  // 发送请求
-  superagent
-      .post("http://www.pushplus.plus/send/")
-      .send(data)
-      .end((err, res) => {
-        if (err) {
-          logger.error(`pushPlus 推送失败:${JSON.stringify(err)}`);
-          return;
-        }
-        const json = JSON.parse(res.text);
-        if (json.code !== 200) {
-          logger.error(`pushPlus 推送失败:${JSON.stringify(json)}`);
-        } else {
-          logger.info("pushPlus 推送成功");
-        }
-      });
+    // 如果没有配置 pushPlus 的 token，则不执行推送
+    if (!(pushPlus.token)) {
+        return;
+    }
+    // 请求体
+    const data = {
+        token: pushPlus.token,
+        title: title,
+        content: desp
+    };
+    // 发送请求
+    superagent
+        .post("http://www.pushplus.plus/send/")
+        .send(data)
+        .end((err, res) => {
+            if (err) {
+                logger.error(`pushPlus 推送失败:${JSON.stringify(err)}`);
+                return;
+            }
+            const json = JSON.parse(res.text);
+            if (json.code !== 200) {
+                logger.error(`pushPlus 推送失败:${JSON.stringify(json)}`);
+            } else {
+                logger.info("pushPlus 推送成功");
+            }
+        });
 };
 
 const push = (title, desp) => {
-  pushServerChan(title, desp);
-  pushTelegramBot(title, desp);
-  pushWecomBot(title, desp);
-  pushWxPusher(title, desp);
-  pushPlusPusher(title, desp);
+    pushServerChan(title, desp);
+    pushTelegramBot(title, desp);
+    pushWecomBot(title, desp);
+    pushWxPusher(title, desp);
+    pushPlusPusher(title, desp);
 };
 
 // 开始执行程序
 async function main() {
-  //用于统计实际容量变化
-  const userSizeInfoMap = new Map();
-  for (let index = 0; index < accounts.length; index += 1) {
-    const account = accounts[index];
-    const { userName, password } = account;
-    if (userName && password) {
-      const userNameInfo = mask(userName, 3, 7);
-      try {
-        logger.log(`账户 ${userNameInfo}开始执行`);
-        const cloudClient = new CloudClient(userName, password);
-        await cloudClient.login();
-        const beforeUserSizeInfo = await cloudClient.getUserSizeInfo();
-        userSizeInfoMap.set(userName, {
-          cloudClient,
-          userSizeInfo: beforeUserSizeInfo,
-        });
-        const result = await doUserTask(cloudClient);
-        result.forEach((r) => logger.log(r));
-        const familyResult = await doFamilyTask(cloudClient);
-        familyResult.forEach((r) => logger.log(r));
-      } catch (e) {
-        logger.error(e);
-        if (e.code === "ETIMEDOUT") {
-          throw e;
+    //用于统计实际容量变化
+    const userSizeInfoMap = new Map();
+    for (let index = 0; index < accounts.length; index += 1) {
+        const account = accounts[index];
+        const {
+            userName,
+            password
+        } = account;
+        if (userName && password) {
+            const userNameInfo = mask(userName, 3, 7);
+            try {
+                logger.log(`账户 ${userNameInfo}开始执行`);
+                const cloudClient = new CloudClient(userName, password);
+                await cloudClient.login();
+                const beforeUserSizeInfo = await cloudClient.getUserSizeInfo();
+                userSizeInfoMap.set(userName, {
+                    cloudClient,
+                    userSizeInfo: beforeUserSizeInfo,
+                });
+                const result = await doUserTask(cloudClient);
+                result.forEach((r) => logger.log(r));
+                const familyResult = await doFamilyTask(cloudClient);
+                familyResult.forEach((r) => logger.log(r));
+            } catch (e) {
+                logger.error(e);
+                if (e.code === "ETIMEDOUT") {
+                    throw e;
+                }
+            } finally {
+                logger.log(`账户 ${userNameInfo}执行完毕`);
+            }
         }
-      } finally {
-        logger.log(`账户 ${userNameInfo}执行完毕`);
-      }
     }
-  }
 
-  //数据汇总
-  for (const [userName, { cloudClient, userSizeInfo }] of userSizeInfoMap) {
-    const userNameInfo = mask(userName, 3, 7);
-    const afterUserSizeInfo = await cloudClient.getUserSizeInfo();
-    logger.log(`账户 ${userNameInfo}实际容量变化:`);
-    logger.log(
-      `个人总容量增加：${(
+    //数据汇总
+    for (const [userName, {
+            cloudClient,
+            userSizeInfo
+        }] of userSizeInfoMap) {
+        const userNameInfo = mask(userName, 3, 7);
+        const afterUserSizeInfo = await cloudClient.getUserSizeInfo();
+        logger.log(`账户 ${userNameInfo}实际容量变化:`);
+        logger.log(
+            `个人总容量增加：${(
         (afterUserSizeInfo.cloudCapacityInfo.totalSize -
           userSizeInfo.cloudCapacityInfo.totalSize) /
         1024 /
@@ -275,17 +324,17 @@ async function main() {
         1024 /
         1024
       ).toFixed(2)}M`
-    );
-  }
+        );
+    }
 }
 
 (async () => {
-  try {
-    await main();
-  } finally {
-    const events = recording.replay();
-    const content = events.map((e) => `${e.data.join("")}`).join("  \n");
-    push("天翼云盘自动签到任务", content);
-    recording.erase();
-  }
+    try {
+        await main();
+    } finally {
+        const events = recording.replay();
+        const content = events.map((e) => `${e.data.join("")}`).join("  \n");
+        push("天翼云盘自动签到任务", content);
+        recording.erase();
+    }
 })();
---
+++ src/push/ntfy.js
@@ -0,0 +1,7 @@
+module.exports = {
+    //4 参数
+    url: process.env.NTFY_URL || "",
+    topic: process.env.NTFY_TOPIC || "",
+    username: process.env.NTFY_USERNAME || "",
+    password: process.env.NTFY_PASSWORD || ""
+};
